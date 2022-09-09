a7a06eed8d2c9da2053ab9e5fba4e08bb9ffcd56
SharapaGorg <sharapov.savely@yandex.ru>
2022-09-09 19:59:15 +0300

    + moderators/phrases database integration

---
+++ database/moderators.py
@@ -0,0 +1,50 @@
+"""
+
+Methods to interact with table named moderators
+
+- get_moderators(user_id : str = None, username : str = None)
+- add_moderator(user_id : str, username : str)
+- delete_property(user_id : str = None, username : str = None)
+
+"""
+
+from sqlalchemy import *
+
+from controller import Session
+from models import Moderator
+
+def get_moderators(user_id : str = None, username : str = None) -> list[Moderator]:
+    _session = Session()
+
+    moders = select(Moderator).where(Moderator.user_id == user_id)
+
+    if username is not None:
+        moders = moders.where(Moderator.username == username)
+
+    result = list(_session.scalars(moders))
+    return result
+
+def add_moderator(user_id : str, username : str) -> Moderator:
+    moder = Moderator(
+        user_id = user_id,
+        username = username
+    )
+
+    _session = Session()
+
+    _session.add(moder)
+    _session.commit()
+
+    _session.close()
+
+    return moder
+
+def delete_moderator(user_id : str = None, username : str = None) -> None:
+    _session = Session()
+    moders = get_moderators(user_id, username)
+
+    for moder in moders:
+        _session.delete(moder)
+        _session.commit()
+
+    _session.close()
--- handlers/moderators.py
+++ handlers/moderators.py
@@ -8,3 +8,7 @@ from utils import *
 @dp.message_handler(commands=['dictionary'])
 async def moderate_dictionary(message : Message, state : FSMContext):
     await message.answer("Ты уверен, что ты модератор, гений?..")
+
+
+async def check_moderator(moderator : User):
+    pass
